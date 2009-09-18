a6bdb550c2c7cbfbec85f178bcb92b9194fda8fa
Jon Noble <jonnobleuk@gmail.com>
2009-09-18 19:34:54 +0100

    Still getting nowhere

--- ui/wiki.py
+++ ui/wiki.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import urllib
 #from lxml import etree
-from lxml.html import document_fromstring
-from lxml import etree
-from StringIO import StringIO
+#from lxml.html import fromstring, parse
+#from lxml.html.soupparser import fromstring
+#from lxml import etree
+#from StringIO import StringIO
+from BeautifulSoup import BeautifulSoup
 
 class Wiki:
     def __init__(self):
@@ -19,21 +21,20 @@ class Wiki:
     # Oh jesus this is hard.
     def treat(self, html):
 #        <div id="bodyContent">
-        parser = etree.XMLParser(ns_clean=True)
-        tree = etree.parse(StringIO(html), parser)
-        tree = etree.tostring(tree.getroot())
+#        parser = etree.XMLParser(ns_clean=True)
+#        tree = etree.parse(StringIO(html), parser)
+#        tree = etree.tostring(tree.getroot())
         
 #        print tree[0:100]
         
-        tree = document_fromstring(tree)
-        
-        print tree.find_class("html")
+#        tree = fromstring(html)
+#        
+#        print tree.get_element_by_id("bodyContent")
         
 #        events = ("start", "end")
-#        context = etree.iterparse(StringIO(html), events=events, tag="html")
-#        
+#        context = etree.iterparse(html, tag="div")
 #        for action, elem in context:
-#            print "%s: %s" % (action.elem.tag)
+#            print "%s: %s" % (action, elem.tag)
             
 #        return etree.tostring(tree.getroot())
 
@@ -43,7 +44,13 @@ class Wiki:
 #        startPos = html.find(start)
 #        print startPos
 
+#        root = fromstring(html)
+
+        tree = BeautifulSoup(html)
         
+        print type(html)
+        print "the" in html        
+        print tree.find(text="the")
         
         
         
