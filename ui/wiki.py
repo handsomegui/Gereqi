610f73c88c395c8fd37a4105d397520f057630e1
Jon Noble <jonnobleuk@gmail.com>
2009-09-18 14:48:04 +0100

    pycurl implementation improvements

--- ui/wiki.py
+++ ui/wiki.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pycurl
-#import mmap
+import StringIO
 
 class Wiki:
     def __init__(self):
@@ -8,24 +8,21 @@ class Wiki:
         
     # This is very bad
     def fetch(self, url):
-#        dataNow = mmap.mmap(-1, 13)
-        dataNow = open("/tmp/amaroqwiki", "wb")
-#        dataNow = "" # This has to be a file
         print url
-        url = "http://www.google.com"
+        dataNow = StringIO.StringIO()
         data = pycurl.Curl()
-        data.setopt(data.URL, url)
+        data.setopt(pycurl.URL, url)
         data.setopt(pycurl.USERAGENT, "Firefox/3.0.10")
         data.setopt(pycurl.FOLLOWLOCATION, 1)
         data.setopt(pycurl.MAXREDIRS, 5)
         data.setopt(pycurl.CONNECTTIMEOUT, 30)
         data.setopt(pycurl.TIMEOUT, 300)
         data.setopt(pycurl.NOSIGNAL, 1)
-        data.setopt(pycurl.WRITEDATA, dataNow)
+        data.setopt(pycurl.WRITEFUNCTION, dataNow.write)
         data.perform()
         
-        dataNow.close()
-        dataNow = open("/tmp/amaroqwiki", "rb")
-        print dataNow.read()
+        html = dataNow.getvalue()
+        return self.treat(html)
         
-        
+    def treat(self, html):
+        return html
