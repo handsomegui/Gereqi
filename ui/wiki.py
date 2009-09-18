634b101f958bd183bfc3d9aa2fc4dc0de19423a9
Jon Noble <jonnobleuk@gmail.com>
2009-09-18 14:53:49 +0100

    Discovered urllib

--- ui/wiki.py
+++ ui/wiki.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
-import pycurl
-import StringIO
+import urllib
 
 class Wiki:
     def __init__(self):
         return
         
-    # This is very bad
     def fetch(self, url):
         print url
-        dataNow = StringIO.StringIO()
-        data = pycurl.Curl()
-        data.setopt(pycurl.URL, url)
-        data.setopt(pycurl.USERAGENT, "Firefox/3.0.10")
-        data.setopt(pycurl.FOLLOWLOCATION, 1)
-        data.setopt(pycurl.MAXREDIRS, 5)
-        data.setopt(pycurl.CONNECTTIMEOUT, 30)
-        data.setopt(pycurl.TIMEOUT, 300)
-        data.setopt(pycurl.NOSIGNAL, 1)
-        data.setopt(pycurl.WRITEFUNCTION, dataNow.write)
-        data.perform()
+
+        html = urllib.urlopen(url)
+        html = html.read()
         
-        html = dataNow.getvalue()
-        return self.treat(html)
+        print html
         
     def treat(self, html):
         return html
