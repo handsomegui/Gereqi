60e127aeb6b783288a7d049c616d5e8d86b464b7
Jon Noble <jonnobleuk@gmail.com>
2009-09-19 17:59:15 +0100

    Created a playlist TABLE in the database

--- ui/database.py
+++ ui/database.py
@@ -20,13 +20,24 @@ class media:
         # using filename as PRIMARY KEY to prevent multiple entries
         self.mediaCurs.execute('''
             CREATE TABLE IF NOT EXISTS media (
-                filename    TEXT PRIMARY KEY ON CONFLICT IGNORE,
+                filename    TEXT ,
                 track    TINYINT(2),
                 title   VARCHAR(50),
                 artist  VARCHAR(50),
                 album   VARCHAR(50),
                 year    SMALLINT(4),
-                genre   VARCHAR(50)
+                genre   VARCHAR(50),
+                PRIMARY KEY (filename) ON CONFLICT IGNORE
+                )
+                ''')
+                
+        self.mediaCurs.execute('''
+            CREATE TABLE IF NOT EXISTS playlist (
+                id   SMALLINT UNSIGNED IDENTITY (1, 1),
+                name    VARCHAR(20),
+                filename    TEXT,
+                track  SMALLINT(3),
+                PRIMARY KEY (id)
                 )
                 ''')
                 
@@ -36,7 +47,7 @@ class media:
         """
         Here we add data into the media database
         """
-        values = ''' "%s","%s","%s","%s", "%s","%s","%s" ''' % (p[0], p[1], p[2], p[3], p[4], p[5], p[6])
+        values = ''' "%s","%s","%s","%s", "%s","%s","%s" ''' % (p[0], p[1], p[2], p[3], p[4], p[5], p[6]) #FIXME:ugly
         cols = "filename,track,title,artist,album,year,genre"
         query = "INSERT INTO media (%s) VALUES (%s)" % (cols, values)
         
