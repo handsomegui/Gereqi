6207483b76e818ac9bdf3640d8b54c75cbe5c18e
test <test@test-laptop.(none)>
2009-10-04 15:50:00 +0100

    Fixed issue(12)

--- amaroQ.py
+++ amaroQ.py
@@ -5,11 +5,16 @@ More things could be done in here but i've
 no idea what yet.
 """
 from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QString
 from ui.amaroq import MainWindow
+
+VERSION = "development"  
 
 def main():
     import sys
     app = QApplication(sys.argv)
+    app.setApplicationName(QString("Amaroq"))
+    app.setApplicationVersion(QString(VERSION))
     wnd = MainWindow()
     wnd.show()
     sys.exit(app.exec_())
