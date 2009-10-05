e7528c3ec45fbc640a09882f00b2018366cc6cf7
Jon Noble <jonnobleuk@gmail.com>
2009-10-05 12:46:00 +0100

    Added the bugfix for issue(12) master branch. v0.1.3

--- amaroQ.py
+++ amaroQ.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QString
 from ui.amaroq import MainWindow
+
+VERSION = "0.1.3"
 
 def main():
     import sys
     app =QApplication(sys.argv)
+    app.setApplicationName(QString("Amaroq"))
+    app.setApplicationVersion(QString(VERSION))
     wnd = MainWindow()
     wnd.show()
     sys.exit(app.exec_())
