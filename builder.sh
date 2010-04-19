48275344d9dedec7b058ba95ee2f41aaefbc7c25
Jon Noble <jonnobleuk@gmail.com>
2010-04-20 00:03:33 +0100

    Seems 'setup.py clean' doesn't actually clean up orphaned files. This edit makes sure of that.

--- builder.sh
+++ builder.sh
@@ -3,15 +3,18 @@
 
 CONTROL="$1"
 DEBNAME="$2"
-python setup.py clean
-python setup.py build
+
 
 rm -rfv debs/
+rm -rfv build/
 mkdir -pv debs/DEBIAN
 mkdir -pv debs/usr/local/bin
 mkdir -pv debs/usr/local/lib/python2.6/dist-packages/gereqi
 mkdir -pv debs/usr/share/applications/
 mkdir -pv debs/usr/share/pixmaps/
+
+python setup.py clean
+python setup.py build
 
 cp -v "$CONTROL"    debs/DEBIAN/
 cp -v build/scripts*/*  debs/usr/local/bin/
