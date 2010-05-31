42a29adfc9af5c2066a5e6d62e213d5a90501e53
Jon Noble <jonnobleuk@gmail.com>
2010-05-31 13:26:54 +0000

    Checking a folder also recursively checks the rest below it.

--- gereqi/configuration.py
+++ gereqi/configuration.py
@@ -11,14 +11,26 @@ from Ui_configuration import Ui_settings_dialog
 
 class MyQDirModel(QDirModel):
     checked = []
+    unchecked = []
         
     def data(self, index, role = Qt.DisplayRole):
         if index.isValid() and (index.column() == 0) and (role == Qt.CheckStateRole):
+            dir_now = self.filePath(index)
+            par_dir = dir_now.split("/")[:-1].join("/")
             # the item is checked only if we have stored its path
-            if self.filePath(index) in MyQDirModel.checked:
-                return Qt.Checked
-            else:
+            if dir_now in MyQDirModel.unchecked:
                 return Qt.Unchecked
+            elif par_dir in MyQDirModel.unchecked:
+                return Qt.Unchecked
+                
+            else:
+                if dir_now in MyQDirModel.checked:
+                    return Qt.Checked
+                elif par_dir in MyQDirModel.checked:
+                    MyQDirModel.checked.append(dir_now)
+                    return Qt.Checked
+                else:
+                    return Qt.Unchecked
                 
         return QDirModel.data(self, index, role)        
         
@@ -33,9 +45,15 @@ class MyQDirModel(QDirModel):
             # store checked paths, remove unchecked paths
             if (value == Qt.Checked):
                 MyQDirModel.checked.append(self.filePath(index))
+                try:
+                    MyQDirModel.unchecked.remove(self.filePath(index))
+                except ValueError:
+                    # Doesn't exist yet
+                    return True
                 return True
             else:
                 MyQDirModel.checked.remove(self.filePath(index))
+                MyQDirModel.unchecked.append(self.filePath(index))
                 return True
                 
         else:
@@ -58,23 +76,38 @@ class Configuration(QDialog, Ui_settings_dialog):
         """
         Make the fileview the correct type
         """
-        
-        dir_model = MyQDirModel()
+        self.dir_model = MyQDirModel()
         filters = QDir.AllDirs|QDir.Readable|QDir.NoDotAndDotDot
-        dir_model.setFilter(filters)
-        dir_model.setReadOnly(True)
-        self.collection_view.setModel(dir_model) 
+        self.dir_model.setFilter(filters)
+        self.dir_model.setReadOnly(True)
+        self.collection_view.setModel(self.dir_model) 
         self.collection_view.setColumnHidden(1, True)
         self.collection_view.setColumnHidden(2, True)
         self.collection_view.setColumnHidden(3, True)
         self.collection_view.expandToDepth(0)
-    
-
-
     
     @pyqtSignature("QAbstractButton*")
     def on_buttonBox_clicked(self, button):
         """
         Slot documentation goes here.
         """
-        print button
+        if button.text() == "Apply":
+            print "APPLY"
+    
+    @pyqtSignature("")
+    def on_buttonBox_accepted(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        print "ACCEPT"
+        QDialog.accept(self)
+    
+    @pyqtSignature("")
+    def on_buttonBox_rejected(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        print "REJECTED"
+        QDialog.reject(self)
