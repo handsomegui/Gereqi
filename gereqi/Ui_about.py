9452cd3d5e106e9d6d5f05bbb21bf62882648531
Jon Noble <regomodo@gmail.com>
2010-09-10 23:25:46 +0000

    Made the about box's text-background transparent. The link opens in the default web browser

--- gereqi/Ui_about.py
+++ gereqi/Ui_about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Fri Sep 10 22:43:26 2010
+# Created: Fri Sep 10 23:16:30 2010
 #      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,26 +12,28 @@ from PyQt4 import QtCore, QtGui
 class Ui_About(object):
     def setupUi(self, About):
         About.setObjectName("About")
-        About.resize(240, 320)
+        About.resize(253, 309)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/Icons/app.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         About.setWindowIcon(icon)
         self.gridLayout = QtGui.QGridLayout(About)
         self.gridLayout.setObjectName("gridLayout")
-        self.textBrowser = QtGui.QTextBrowser(About)
-        self.textBrowser.setAutoFillBackground(True)
-        self.textBrowser.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.textBrowser.setFrameShape(QtGui.QFrame.NoFrame)
-        self.textBrowser.setFrameShadow(QtGui.QFrame.Plain)
-        self.textBrowser.setLineWidth(0)
-        self.textBrowser.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
-        self.textBrowser.setObjectName("textBrowser")
-        self.gridLayout.addWidget(self.textBrowser, 0, 0, 1, 1)
         self.buttonBox = QtGui.QDialogButtonBox(About)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Close)
         self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
+        self.textBrowser = QtGui.QTextBrowser(About)
+        self.textBrowser.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        self.textBrowser.setFrameShape(QtGui.QFrame.NoFrame)
+        self.textBrowser.setFrameShadow(QtGui.QFrame.Plain)
+        self.textBrowser.setTabChangesFocus(True)
+        self.textBrowser.setAcceptRichText(False)
+        self.textBrowser.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse)
+        self.textBrowser.setOpenExternalLinks(True)
+        self.textBrowser.setOpenLinks(False)
+        self.textBrowser.setObjectName("textBrowser")
+        self.gridLayout.addWidget(self.textBrowser, 0, 0, 1, 1)
 
         self.retranslateUi(About)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), About.accept)
@@ -45,13 +47,15 @@ class Ui_About(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Droid Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"right\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><img src=\":/Icons/app.png\" /></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:600;\"></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:18pt; font-weight:600;\"></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; font-weight:600;\">Gereqi</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">version 0.4.2</p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">© 2009,2010 Contributors</p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Visit <a href=\"http://code.google.com/p/gereqi/\"><span style=\" text-decoration: underline; color:#e85290;\">http://code.google.com/p/gereqi/</span></a></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
 import resource_rc
--- gereqi/about.py
+++ gereqi/about.py
@@ -4,7 +4,7 @@
 Module implementing Equaliser.
 """
 
-from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QDialog, QColor, QDesktopServices
 from PyQt4.QtCore import pyqtSignature
 
 from Ui_about import Ui_About
@@ -19,5 +19,10 @@ class About(QDialog, Ui_About):
         """
         QDialog.__init__(self, parent)
         self.setupUi(self)
-    
+        self.textBrowser.anchorClicked.connect(self.__linker)
+        
 
+    
+    def __linker(self, url):
+        desk = QDesktopServices()
+        desk.openUrl(url)
