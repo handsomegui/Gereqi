de29b240c0ef5e0d09168c11e05922d1db8c27d9
Jon Noble <regomodo@gmail.com>
2010-09-22 20:49:57 +0000

    Moving code to it's own file.

---
+++ gereqi/playlist.py
@@ -0,0 +1,253 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+
+class Playlist:
+    def __init__(self, parent):
+        self.ui_main = parent          
+  
+    def __sort_custom(self, mode="FileName"):
+        """
+        Puts the playlist into a sorting order
+        that works only for filename sorting (the only
+        order that appears to work)
+        """
+        # Finds the sorting status of the playlist
+        hdr = self.ui_main.track_tbl.horizontalHeader()
+        self.sort_order = hdr.sortIndicatorOrder()
+        self.sort_pos = hdr.sortIndicatorSection()
+        fname_pos = self.header_search(mode)
+        
+        # Not the default FileName+ascending sort
+        if (self.sort_pos != fname_pos) or (self.sort_order != 0) :
+            hdr.setSortIndicator(fname_pos, 0)
+            
+    def __unsort(self):
+        """
+        Puts the playlist sorting back to what it was 
+        """
+        fname_pos = self.header_search("FileName")
+        if self.sort_pos != fname_pos or (self.sort_order != 0):
+            hdr = self.ui_main.track_tbl.horizontalHeader()
+            hdr.setSortIndicator(self.sort_pos, self.sort_order)
+    
+    def add_list_to_playlist(self, tracks):
+        """
+        Takes a list of filenames and adds to the playlist
+        whilst handling the sorting orders
+        """
+        self.__sort_custom()
+        for trk in tracks:
+            # This is for adding a track which has info attached in a tuple
+            if isinstance(trk, tuple):
+                self.add_to_playlist(trk[0], trk[1])
+            else:
+                self.add_to_playlist(trk, None)        
+        self.__unsort()
+    
+    def add_to_playlist(self, file_name, info=None):
+        """
+        Called when adding tracks to the playlist either locally
+        or from the database. Does not pull metadata from 
+        the database and is passed into the function directly
+        """
+        # This allows to manually put in info for things we know
+        # mutagen cannot handle things like urls for podcasts
+        self.ui_main.clear_trktbl_bttn.setEnabled(True)
+        metadata = info
+        if metadata is None:
+            # see if the track is already in db
+            metadata = self.ui_main.media_db.get_info(file_name)
+            if metadata is None:
+                # get the info using the tag-extractor module
+                metadata = self.ui_main.meta.extract(str(file_name))
+                if metadata is None:
+                    return
+                else:       
+                    trk = QString("%1").arg(metadata[5].toInt()[0], 2, 10, QChar('0'))
+                    metadata = {"Track": trk,  "Title": metadata[0],
+                                "Artist": metadata[1], "Album": metadata[2],
+                                "Year":metadata[3], "Genre": metadata[4],
+                                "Length": metadata[6], "Bitrate": metadata[7], 
+                                "FileName": file_name}
+            else:
+                trk = QString("%1").arg(metadata[6].toInt()[0], 2,10, QChar('0'))
+                metadata = {'Track': trk, "Title": metadata[1], "Artist": metadata[2], 
+                            "Album": metadata[3], "Year": metadata[4], "Genre": metadata[5],
+                            "Length": metadata[7], "Bitrate": metadata[8], 
+                            "FileName": file_name}
+                                    
+        row = self.ui_main.track_tbl.rowCount()
+        self.ui_main.track_tbl.insertRow(row)
+        # Creates each cell for a track based on info
+        for key in metadata:
+            tbl_wdgt = QTableWidgetItem(metadata[key])
+            column = self.header_search(key)
+            self.ui_main.track_tbl.setItem(row, column, tbl_wdgt)
+        self.ui_main.track_tbl.resizeColumnsToContents()   
+        
+        
+    # This is needed as the higlighted row can be different
+    # than the currentRow method of Qtableview.
+    def current_row(self):
+        """
+        Finds the playlist row of the
+        currently playing track
+        """
+        file_list = self.gen_file_list()
+        current_file = self.ui_main.player.audio_object.current_source()
+        
+        if current_file is None:
+            return self.ui_main.track_tbl.currentRow()
+        elif current_file in file_list:
+            return file_list.index(current_file)
+        
+        
+    def gen_file_list(self):
+        """
+        Creates a list of files in the playlist at its
+        current sorting top to bottom
+        """
+        rows = self.ui_main.track_tbl.rowCount() 
+        column = self.header_search("FileName")
+        file_list = [self.ui_main.track_tbl.item(row, column).text()
+                        for row in range(rows)]
+        return file_list           
+        
+    def del_track(self):
+        """
+        Deletes selected tracks from playlist
+        """
+        items = self.ui_main.track_tbl.selectedItems()
+        for item in items:
+            try:
+                row = item.row()
+                self.ui_main.track_tbl.removeRow(row)                
+                self.tracknow_colourise(self.current_row())
+            except RuntimeError:
+                # likely deleted already 
+                # i.e selected same row but multiple columns
+                return 
+        
+    def header_search(self, val):
+        """
+        This will eventually allows the column order of the 
+        playlist view to be changed         
+        """
+        cols = self.ui_main.track_tbl.columnCount()
+        headers = [self.ui_main.track_tbl.horizontalHeaderItem(col).text() 
+                   for col in range(cols)]
+        return headers.index(val)
+        
+    def tracknow_colourise(self, now=None):
+        """
+        Instead of using QTableWidget's selectRow function, 
+        set the background colour of each item in a row
+        until track changes.
+        """
+        columns = self.ui_main.track_tbl.columnCount()
+        rows = self.ui_main.track_tbl.rowCount()
+        palette = self.ui_main.track_tbl.palette()
+        
+        for row in range(rows):
+            for col in range(columns):
+                item = self.ui_main.track_tbl.item(row, col)
+                if row != now:
+                    if row % 2:
+                        # Odd-row
+                        item.setBackgroundColor(palette.alternateBase().color())
+                    else:
+                        # even row
+                        item.setBackgroundColor(palette.base().color())
+                else:
+                    highlight = palette.highlight().color()
+                    highlight.setAlpha(128)
+                    item.setBackgroundColor(highlight)
+                    self.ui_main.track_tbl.selectRow(now)
+                        
+    def highlighted_track(self):
+        """
+        In the playlist
+        """
+        row = self.ui_main.track_tbl.currentRow()
+        column = self.header_search("FileName")
+        track = None
+        # -1 is the row value for None
+        if row > -1:
+            track = self.ui_main.track_tbl.item(row, column).text()
+        return track        
+        
+    def clear(self):
+        """
+        Clears the playlist
+        """
+        self.ui_main.track_tbl.clearContents()
+        rows = self.ui_main.track_tbl.rowCount()
+        # For some reason can only remove from bot to top
+        for cnt in range(rows, -1, -1):
+            self.ui_main.track_tbl.removeRow(cnt)
+            
+    def gen_full_list(self):
+        """
+        Get all the info in the playlist
+        """
+        rows = self.ui_main.track_tbl.rowCount()
+        columns = self.ui_main.track_tbl.columnCount()
+        headers = [self.ui_main.track_tbl.horizontalHeaderItem(cnt).text()
+                        for cnt in range(columns)]
+        tracks = []
+        for row in range(rows):
+            tmp_list = []
+            for col in range(columns):
+                tmp_list.append(self.ui_main.track_tbl.item(row, col).text())
+            tracks.append(tmp_list)        
+        return headers, tracks
+     
+    def track_sorting(self, index):
+        """
+        A bit of a hack. The table is first sorted by track automatically
+        then manually sorting by album by telling the table to auto-sort
+        it that way
+        """
+        hdrs, tracks = self.gen_full_list()
+        if hdrs[index] == "Track":
+            self.__sort_custom("Album")
+        self.tracknow_colourise(self.current_row())
+        
+    def gen_track_list(self):
+        """
+        Get a list of filenames in playlist
+        """
+        rows = self.ui_main.track_tbl.rowCount()
+        trk_col = self.header_search("FileName")
+        tracks = []
+        for row in range(rows):
+            tracks.append(self.ui_main.track_tbl.item(row, trk_col).text())
+        return tracks
+        
+        
+class PlaylistHistory:
+    """
+    The playlist history
+    """
+    stack = []
+    position = 0
+    
+    def update(self, tracks):
+        if PlaylistHistory.position != len(PlaylistHistory.stack):
+            del PlaylistHistory.stack[PlaylistHistory.position:]
+            
+        PlaylistHistory.stack.append(tracks)
+        PlaylistHistory.position += 1
+        
+    def last_list(self):
+        if PlaylistHistory.position > 0:
+            PlaylistHistory.position -= 1
+            last = PlaylistHistory.position == 0
+            return PlaylistHistory.stack[PlaylistHistory.position], last
+        
+    def next_list(self):
+        if PlaylistHistory.position < len(PlaylistHistory.stack):
+            PlaylistHistory.position += 1
+            first = PlaylistHistory.position == (len(PlaylistHistory.stack) - 1)
+            return PlaylistHistory.stack[PlaylistHistory.position], first
